555ef770282c80c8ec776a579df6704019ec4f1b
kimwnd <gwcm@naver.com>
2019-02-27 11:20:22 +0900

    settings ip 주소 추가

--- humesh/settings.py
+++ humesh/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.8.8']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.8.8', '15.164.47.154']
 
 # Application definition
 
