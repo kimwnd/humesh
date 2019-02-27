36c9c66255dac36ce4e37f5dc8a2917517388536
kimwnd <gwcm@naver.com>
2019-02-27 14:23:23 +0900

    static file

--- humesh/settings.py
+++ humesh/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), '/home/ubuntu/hucon/static',]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
