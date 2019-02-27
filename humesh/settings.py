7262933449006bd28a5be017a1a2fbde16392fc0
kimwnd <gwcm@naver.com>
2019-02-27 13:19:43 +0900

    shome.html 수정

--- humesh/settings.py
+++ humesh/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env() # reading .env file
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.8.8', '15.164.47.154']
 
