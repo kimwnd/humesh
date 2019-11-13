0e4f75d9c57340e523f113c04d40f849ecaf039a
kimwnd <gwcm@naver.com>
2019-11-13 20:07:29 +0900

    fix votage #1

--- mesh/views.py
+++ mesh/views.py
@@ -177,6 +177,7 @@ def test_notification(request):
         logger.debug("data: {}".format(data))
         f = open('demo1.txt', 'a')
         f.write('POST data is new added\n\n')
+        f.write("data: {}".format(data))
         f.write(str(data))
         f.write('published_at : {}'.format(data['published_at']))
         f.close()
