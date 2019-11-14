e8e603dc703a8aa778a4afad92475f0fb2ff6937
kimwnd <gwcm@naver.com>
2019-11-14 18:00:20 +0900

    aws iot test 8

--- mesh/views.py
+++ mesh/views.py
@@ -182,7 +182,7 @@ def test_notification(request):
         f.write("----\n\n")
         received = (request.body).decode('ascii')
         received = ast.literal_eval(received)
-        f.write(str(received['Group']))
+        f.write(str(received['data']['Humidity']))
         f.write("----\n\n")
         f.write(str(type(data)))
         f.write("----\n\n")
