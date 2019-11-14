279e2d1db5edd9ccfccef6c01dfa7c86d17b79e9
kimwnd <gwcm@naver.com>
2019-11-14 16:56:23 +0900

    aws iot test 1

--- mesh/views.py
+++ mesh/views.py
@@ -178,8 +178,9 @@ def test_notification(request):
         f = open('demo1.txt', 'a')
         f.write('POST data is new added\n\n')
         f.write("{}".format(request.body))
-        f.write("----")
+        f.write("----\n")
         f.write(str(data))
+        f.write("----\n")
         # f.write('published_at : {}'.format(data['published_at']))
         f.close()
 
