136895e38c0607aada4eb5086b26f620660fd6f0
kimwnd <gwcm@naver.com>
2020-01-10 16:10:29 +0900

    map/test 수정

--- mesh/views.py
+++ mesh/views.py
@@ -2618,8 +2618,8 @@ class MapTestview(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        data = CatM1SensorDataMdodel.objects.order_by('-created').values('id', 'device_name', 'created')[:10]
-        locs = CatM1LocationMdodel.objects.order_by('-created').values('id', 'device_name', 'latitude', 'longitude', 'created')[:10]
+        data = CatM1SensorDataMdodelNew.objects.order_by('-created').values('id', 'device_name', 'created')[:10]
+        locs = CatM1LocationMdodelNew.objects.order_by('-created').values('id', 'device_name', 'latitude', 'longitude', 'created')[:10]
 
         sensor_data = [ (d['id'], d['device_name'], str(d['created']+datetime.timedelta(hours=9))[:19] ) for d in data ]
         loc_data = [ (l['id'], l['device_name'], l['latitude'], l['longitude'], str(l['created']+datetime.timedelta(hours=9))[:19] ) for l in locs ]
